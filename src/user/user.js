ba53416285679ef231d430e69c1a3d64de31d705
Rishabh Anand <ranand16@gmail.com>
2017-02-09 15:07:53 +0530

    before all the controller part

--- src/user/user.js
+++ src/user/user.js
@@ -1,10 +1,12 @@
 
 import _ from 'lodash';
 
-export default function($scope, userFactory){
+export default function($scope, $location, userFactory){
+
 
   userFactory.getUsers($scope);
   $scope.createUser = _.partial(userFactory.createUser,$scope);
   $scope.userLogin  = _.partial(userFactory.userLogin,$scope);
+  
 
 }
